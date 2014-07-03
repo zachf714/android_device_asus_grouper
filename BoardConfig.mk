2f4e9d089638870edead9a4499d4fd53bbd6fef9
Zach <zachf714@gmail.com>
2014-07-03 16:36:56 -0400

    Revert "Add initial TWRP config"
    This reverts commit 916fb19f92a051c4576aa1ea69123aaf576bb196.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,10 +61,3 @@ BOARD_SEPOLICY_UNION := \
         surfaceflinger.te \
         system.te \
         zygote.te
-
-#TWRP
-DEVICE_RESOLUTION := 800x1280
-BOARD_HAS_NO_REAL_SDCARD := true
-RECOVERY_SDCARD_ON_DATA := true
-TW_NO_USB_STORAGE := false
-TW_INCLUDE_JB_CRYPTO := true
